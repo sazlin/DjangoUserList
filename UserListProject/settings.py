1887f3a5410683310f5b47c0bc46fc92c48e1b4f
sazlin <sazlin@gmail.com>
2014-05-04 18:24:50 -0700

    uncommented database object, which is kind of important...

--- UserListProject/settings.py
+++ UserListProject/settings.py
@@ -52,7 +52,7 @@ WSGI_APPLICATION = 'UserListProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-#DATABASES = {
+DATABASES = {}
 #    'default': {
 #        'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
